6cc8a850cb23f419a713bdd51b2de61ff8596fd2
Subhash Ravindra SRIVATSA <subhashrsrivatsa.paris@gmail.com>
2020-05-18 16:50:26 +0530

    Added new PDFParser, and a sample pdf document inside the resource.

---
+++ AIRecruiter/src/main/java/com/java/ai/pdfparser/PDFParser.java
@@ -0,0 +1,40 @@
+package com.java.ai.pdfparser;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+public class PDFParser {
+
+	public static void main(String[] args) throws IOException{
+
+		try (PDDocument document = PDDocument.load(new File("C:/Users/subha/git/AI-Recruiter/AIRecruiter/src/main/resources/PDF_Docs/sample.pdf"))) {
+
+            document.getClass();
+
+            if (!document.isEncrypted()) {
+
+                PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+                stripper.setSortByPosition(true);
+
+                PDFTextStripper tStripper = new PDFTextStripper();
+
+                String pdfFileInText = tStripper.getText(document);
+                //System.out.println("Text:" + st);
+
+				// split by whitespace
+                String lines[] = pdfFileInText.split("\\r?\\n");
+                for (String line : lines) {
+                    System.out.println(line);
+                }
+
+            }
+
+        }
+
+	}
+
+}
